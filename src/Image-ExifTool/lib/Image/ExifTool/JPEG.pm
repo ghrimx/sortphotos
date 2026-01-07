58282482ca65b03bb89392e2da6a1135460b5bb4
ghrimx <debruycker.v@gmail.com>
2026-01-07 13:54:34 +0100

    init

--- src/Image-ExifTool/lib/Image/ExifTool/JPEG.pm
+++ src/Image-ExifTool/lib/Image/ExifTool/JPEG.pm
@@ -11,7 +11,7 @@ use strict;
 use vars qw($VERSION);
 use Image::ExifTool qw(:DataAccess :Utils);
 
-$VERSION = '1.39';
+$VERSION = '1.40';
 
 sub ProcessOcad($$$);
 sub ProcessJPEG_HDR($$$);
@@ -211,6 +211,10 @@ sub ProcessJPEG_HDR($$$);
     APP7 => [{
         Name => 'Pentax',
         Condition => '$$valPt =~ /^PENTAX \0/',
+        SubDirectory => { TagTable => 'Image::ExifTool::Pentax::Main' },
+      }, {
+        Name => 'Ricoh',
+        Condition => '$$valPt =~ /^RICOH\0/',
         SubDirectory => { TagTable => 'Image::ExifTool::Pentax::Main' },
       }, {
         Name => 'Huawei',
@@ -565,7 +569,7 @@ sub ProcessJPEG_HDR($$$);
 # APP9 Media Jukebox segment (ref PH)
 %Image::ExifTool::JPEG::MediaJukebox = (
     GROUPS => { 0 => 'XML', 1 => 'MediaJukebox', 2 => 'Image' },
-    VARS => { NO_ID => 1 },
+    VARS => { ID_FMT => 'none' },
     NOTES => 'Tags found in the XML metadata of the APP9 "Media Jukebox" segment.',
     Date => {
         Groups => { 2 => 'Time' },
